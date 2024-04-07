64b7ec24b8de2c608c2835372e5653e8e704e063
truepurplerat <tomtom710710@gmail.com>
2024-04-07 16:37:17 +0200

    Felix gay

---
+++ src/main/java/producers/Whip.java
@@ -0,0 +1,17 @@
+package producers;
+
+public class Whip extends Producer {
+    final int factor = 1;
+
+    @Override
+    public int getfactor() {
+        return factor;
+
+    }
+    @Override
+    public long getprice(){
+        return 69;
+    }
+
+
+}
